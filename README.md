b3a6f35317ba2a158b38b16876531116ed265896
yueyinqiu <lujiaqi2003@126.com>
2022-01-28 13:23:32 +0800

    archiving

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # YiJingFramework.StemsAndBranches.Relationships
+This repository will be archived since it's too complicated to have unified schemes.
+
+We will directly provide some frequently-used relationships in [YiJingFramework.StemsAndBranches wiki](https://github.com/YiJingFramework/YiJingFramework.StemsAndBranches/wiki).
---
+++ src/YiJingFramework.StemsAndBranches.RelationshipsTests/Providers/CommonAttributesProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YiJingFramework.StemsAndBranches.Relationships.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YiJingFramework.StemsAndBranches.Relationships.Providers.Tests
+{
+    [TestClass()]
+    public class CommonAttributesProviderTests
+    {
+        [TestMethod()]
+        public void GetAttributeOfTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetAttributeOfTest1()
+        {
+        }
+    }
+}
